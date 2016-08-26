401ab0251e17c280943a2fc643068f92ce6b2739
pinuslee32 <pinuslee32@gmail.com>
2016-08-26 08:24:54 +0000

    build modify

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 WORK_DIR=$(pwd)
-echo "############## node js build ##############"
+echo "############## node js build[" + $WORK_DIR +"] ##############"
 cd $WORK_DIR/node
 npm install
 
--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ forever stopall
 echo "================================================================="
 echo "   Pinus Server Start "
 echo "================================================================="i
-#forever start -a -l pxboard_forever.log -o log/pxboard_out.log -e log/pxboard_err.log pxboard/bin/www
-#forever start -a -l chatting_forever.log -o log/chatting_out.log -e log/chatting_err.log chatting/bin/www
+forever start -a -l pxboard_forever.log -o log/pxboard_out.log -e log/pxboard_err.log node/bin/px-board.js
+forever start -a -l chatting_forever.log -o log/chatting_out.log -e log/chatting_err.log node/bin/px-chat.js
